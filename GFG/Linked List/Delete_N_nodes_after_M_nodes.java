0065ea7a56b851863587a3fb31581c875d158ee5
Souvik Jana <42837018+sjsouvik@users.noreply.github.com>
2020-06-23 20:44:14 +0530

    Delete N nodes after skipping M nodes in SLL

---
+++ GFG/Linked List/Delete_N_nodes_after_M_nodes.java
@@ -0,0 +1,157 @@
+/*
+Given a linked list, delete N nodes after skipping M nodes of a linked list until the last of the linked list.
+
+Input:
+First line of input contains number of testcases T. For each testcase, first line of input contains number of elements in the linked list and next M and N 
+respectively space separated. The last line contains the elements of the linked list.
+
+Output:
+Function should not print any output to stdin/console.
+
+User Task:
+The task is to complete the function linkdelete() which should modify the linked list as required.
+
+Example:
+Input:
+2
+8
+2 1
+9 1 3 5 9 4 10 1
+6
+6 1
+1 2 3 4 5 6
+
+Output: 
+9 1 5 9 10 1
+1 2 3 4 5 6
+
+Explanation:
+Testcase 1: Deleting one node after skipping the M nodes each time, we have list as 9-> 1-> 5-> 9-> 10-> 1.
+
+**********************************************************************Solution******************************************************************************/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code
+{
+    static Node insert(Node head, int data)
+    {
+        Node temp = new Node(data);
+        if(head == null)
+        {
+          head = temp;
+          return head;
+        }
+        else
+	{
+            Node t = head;
+            while(t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+    
+    static void printList(Node head)
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        
+    }
+
+   public static void main (String[] args)throws IOException 
+   {
+       BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+       int t = Integer.parseInt(read.readLine());
+       
+       while(t-- > 0)
+       {
+           int listSize = Integer.parseInt(read.readLine());
+           String str[] = read.readLine().trim().split(" ");
+           
+           int m = Integer.parseInt(str[0]);
+           int n = Integer.parseInt(str[1]);
+           
+           Node head = null;
+           str = read.readLine().trim().split(" ");
+           for(int i = 0; i < listSize; i++)
+           {
+               head = insert(head, Integer.parseInt(str[i]));
+           }
+          new Solution().linkdelete(head, m, n); 
+          printList(head); 
+          System.out.println();
+       }
+   } 
+}
+
+
+class Solution
+{
+    static void linkdelete(Node head, int m, int n)
+    {        
+        if(m == 0 && n == 1)
+        {
+            while(head != null)
+                head = head.next;
+        }
+        else
+        {
+            Node temp = head;
+            
+            int skip = m;
+            int delete = n;
+            
+            while(temp != null)
+            {
+                if(skip == 1)
+                {
+                    Node ptr = temp;
+                    
+                    while(--delete >= 0 && ptr.next != null)
+                    {
+                        ptr = ptr.next;
+                    }
+                    
+                    temp.next = ptr.next;
+                    
+                    skip = m;
+                    delete = n;
+                }
+                else
+                    skip--;
+                    
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
