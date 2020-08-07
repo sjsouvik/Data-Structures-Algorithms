d524722d0353d7cae9e24dba182f47f88d297cf8
Souvik Jana <42837018+sjsouvik@users.noreply.github.com>
2020-08-07 13:18:38 +0530

    Print all nodes that don't have sibling

---
+++ Amazon-SDE-Test-Series/DSA/Tree/Print-all-nodes-that-don't-have-sibling.java
@@ -0,0 +1,200 @@
+/*
+Given a Binary Tree of size N, such that all nodes have distinct values, print all the nodes which don't have a sibling node in sorted order. If all nodes 
+have a sibling node then print -1 .
+Input:
+First line of input contains the number of test cases T. For each test case, there will be only a single line of input which is a string representing the 
+tree as described below: 
+
+    The values in the string are in the order of level order traversal of the tree where, numbers denotes node values, and a character “N” denotes NULL 
+child.
+
+    For example:
+      
+    For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output:
+For each test case , print all the nodes which don't have a sibling node in the sorted order. If all nodes have a sibling node then print -1.
+Your Task:
+You don't have to take input. Complete the function printSibling() that takes root node of the tree as parameter and prints all the space seperated sorted 
+nodes which don't have a sibling node in the sorted order.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <=T<= 30
+1 <=Number of nodes<= 104
+
+Example:
+Input
+2
+37 20 N 113 
+1 2 3
+Output 
+20 113
+-1
+Explanation:
+Testcase 1: In above example the root node is 37 which has left child 20 and right child nothing. The node 20 has a left child 113. So, the aswer is 2 that 
+is node 20 and 113 don't have siblings.
+
+***********************************************************************Solution**************************************************************************/
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG 
+{    
+    static Node buildTree(String str)
+    {        
+        if(str.length()==0 || str.charAt(0)=='N')
+	{
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+
+        // Push the root to the queue        
+        Queue<Node> queue = new LinkedList<>();         
+        queue.add(root);
+
+        // Starting from the second element        
+        int i = 1;
+
+        while(queue.size()>0 && i < ip.length) 
+	{            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) 
+	    {                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) 
+	    {                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Tree g = new Tree();
+    	        g.printSibling(root);
+    	        System.out.println();
+	        }
+	}
+}
+
+
+class Tree
+{
+    ArrayList<Integer> l = new ArrayList<Integer>();
+    
+    void printSibling(Node node)
+    {
+        print_Sibling(node);
+        
+        if(l.size() == 0)
+            System.out.print(-1);
+        else
+        {
+            Collections.sort(l);
+            
+            for(Integer i : l)
+                System.out.print(i + " ");
+        }
+    }
+    
+	void print_Sibling(Node node)
+	{
+	    if(node == null)
+	        return; 
+	    
+	    /* if this condition satisfies then the current node has only one child, so will add the single child to ArrayList and traverse the rest of the subtree on not null side 
+	    i.e. if the current node has only left child then will add that left child to the ArrayList as that doesn't have any sibling and start traversing left subtree since there's no right child of current node */
+	    if((node.left != null && node.right == null) || (node.left == null && node.right != null))
+	    {
+	        if(node.left != null)
+	        {
+	            l.add(node.left.data);
+	            print_Sibling(node.left); 
+	        }
+	        else
+	        {
+	            l.add(node.right.data);
+	            print_Sibling(node.right);
+	        }
+	        
+	        return;
+	    }
+	    
+	    print_Sibling(node.left);
+	    print_Sibling(node.right);
+	}
+}
+
+
+
+
+
+
+
