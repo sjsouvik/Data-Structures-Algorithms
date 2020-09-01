6c6b5c1ae2a92030d9c9ef65e16c3737717c3203
Souvik Jana <42837018+sjsouvik@users.noreply.github.com>
2020-09-01 21:28:02 +0530

    Find the closest element in BST

---
+++ Amazon-SDE-Test-Series/DSA/Binary Search Tree/Find-the-closest-element-in-bst.java
@@ -0,0 +1,178 @@
+/*
+Given a BST and an integer. Find the least absolute difference between any node value of the BST and the given integer.
+
+Example 1:
+
+Input:
+        10
+      /   \
+     2    11
+   /  \ 
+  1    5
+      /  \
+     3    6
+      \
+       4
+K = 13
+Output: 2
+Explanation: K=13. The node that has
+value nearest to K is 11. so the answer
+is 2
+
+Example 2:
+
+Input:
+      8
+    /   \
+   1     9
+    \     \
+     4    10
+    /
+   3
+K = 9
+Output: 0
+Explanation: K=9. The node that has
+value nearest to K is 9. so the answer
+is 0.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function minDiff() that takes the root of the BST and an integer K as its 
+input and returns the minimum absolute difference between any node value of the BST and the integer K.
+
+Expected Time Complexity: O(Height of the BST).
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= Number of nodes <= 100000
+
+***********************************************************************Solution*****************************************************************************/
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException 
+    {    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Solution T = new Solution();
+            System.out.println(T.maxDiff(root,k));
+            t--;
+        }
+    }
+}
+
+class Solution
+{
+    static int maxDiff(Node  root, int k) 
+    { 
+        Node curr = root;
+        int diff = Integer.MAX_VALUE;
+        
+        while(curr != null)
+        {
+            if(k > curr.data)
+            {
+                diff = Integer.min(diff, k - curr.data);
+                curr = curr.right;
+            }
+            else if(k < curr.data)
+            {
+                diff = Integer.min(diff, curr.data - k);
+                curr = curr.left;
+            }
+            else
+                return 0;
+        }
+        
+        return diff;
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
