da22b4a1d915da658c06da0d503f192defa6bbfd
Souvik Jana <souvikjana2013@gmail.com>
2021-01-06 16:26:13 +0530

    Insert into BST | Medium

---
+++ Leetcode/Tree/701.insert-into-a-binary-search-tree.java
@@ -0,0 +1,81 @@
+/*
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+ 
+Example 1:
+
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+Explanation: Another accepted tree is:
+
+Example 2:
+
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+
+Example 3:
+
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]
+ 
+Constraints:
+
+    The number of nodes in the tree will be in the range [0, 104].
+    -108 <= Node.val <= 108
+    All the values Node.val are unique.
+    -108 <= val <= 108
+    It's guaranteed that val does not exist in the original BST.
+
+***************************************************************************************Solution*******************************************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    //Iterative solution, Time and space complexity   - O(height)
+    public TreeNode insertIntoBST(TreeNode root, int val) 
+    {
+        TreeNode newNode = new TreeNode(val);
+        
+        if(root == null)
+            return newNode;
+        
+        TreeNode parent = null, current = root;                
+        
+        while(current != null)
+        {
+            parent = current;
+            
+            if(val < current.val)
+                current = current.left;
+            
+            else if(val > current.val)
+                current = current.right;
+            
+            //if the value is already present in the BST then there's no need to insert the same again. Howeve, that wouldn't happen here as per the constraints given
+            else
+                return root;
+        }
+                
+        if(val < parent.val)
+            parent.left = newNode;        
+        else //if(val > parent.val)
+            parent.right = newNode;
+        
+        return root;
+    }
+}
