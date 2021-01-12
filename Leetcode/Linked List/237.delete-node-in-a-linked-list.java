ec9d07bb99f22ffcb5f2725ef7cb3b5232059163
Souvik Jana <souvikjana2013@gmail.com>
2021-01-12 15:17:50 +0530

    Delete node in a linked list

---
+++ Leetcode/Linked List/237.delete-node-in-a-linked-list.java
@@ -0,0 +1,59 @@
+/*
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+ 
+Example 1:
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+
+Example 2:
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+
+Example 3:
+
+Input: head = [1,2,3,4], node = 3
+Output: [1,2,4]
+
+Example 4:
+
+Input: head = [0,1], node = 0
+Output: [1]
+
+Example 5:
+
+Input: head = [-3,5,-99], node = -3
+Output: [5,-99]
+ 
+Constraints:
+
+    The number of the nodes in the given list is in the range [2, 1000].
+    -1000 <= Node.val <= 1000
+    The value of each node in the list is unique.
+    The node to be deleted is in the list and is not a tail node
+
+****************************************************************************************Solution********************************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution 
+{
+    //the given node is not a tail node, it would be at beginning or in between somewhere
+    //the idea is to copy the next node's value in the node that needs to be deleted and delete the next node of it
+    public void deleteNode(ListNode node) 
+    {        
+        node.val = node.next.val; //copying the next node's value in the node to be deleted 
+        node.next = node.next.next; //next reference of deleted node would be next reference of its next node                
+    }
+}
