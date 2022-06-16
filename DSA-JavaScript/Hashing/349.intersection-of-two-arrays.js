fb0e0e2fe97ddcc9b061b1ac144233394041924b
Souvik Jana <souvikjana2013@gmail.com>
2022-06-16 19:08:39 +0530

    Intersection of unsorted arrays

---
+++ DSA-JavaScript/Hashing/349.intersection-of-two-arrays.js
@@ -0,0 +1,38 @@
+/*
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+******************************************************************Solution*********************************************************/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1), set2 = new Set(nums2), res = [];
+    
+    for(const item of set1){
+        if(set2.has(item)){
+            res.push(item);
+        }
+    }
+    
+    return res;
+};
