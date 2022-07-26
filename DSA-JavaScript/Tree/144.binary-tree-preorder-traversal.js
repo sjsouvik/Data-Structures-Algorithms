29501dfd1ce320baa8dce713e2d3cfb84bac1fa9
Souvik Jana <souvikjana2013@gmail.com>
2022-07-26 21:07:57 +0530

    Preorder traversal of binary tree

---
+++ DSA-JavaScript/Tree/144.binary-tree-preorder-traversal.js
@@ -0,0 +1,52 @@
+/*
+
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+******************************************************************Solution*************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let res = [];
+    
+    if(!root){
+        return res;
+    }
+    
+    res.push(root.val);
+    res = res.concat(preorderTraversal(root.left));
+    res = res.concat(preorderTraversal(root.right));
+    
+    return res;
+};
