bfa3111643d70c128747e3455915d7dda82f8787
Souvik Jana <souvikjana2013@gmail.com>
2022-10-21 23:51:36 +0530

    Find middle node of the LL

---
+++ DSA-JavaScript/Linked List/876.middle-of-the-linked-list.js
@@ -0,0 +1,48 @@
+/*
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
+***************************************************************Solution*************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function(head) {
+    let fastPointer = head, slowPointer = head;
+    
+    while(fastPointer && fastPointer.next){
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+    
+    return slowPointer;
+};
