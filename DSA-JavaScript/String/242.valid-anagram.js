8030911aa36e824f5f0fdf4c314560d2556ab9f9
Souvik Jana <souvikjana2013@gmail.com>
2023-08-29 19:13:39 +0530

    update solution of determining a valid anagram

--- DSA-JavaScript/String/242.valid-anagram.js
+++ DSA-JavaScript/String/242.valid-anagram.js
@@ -27,23 +27,22 @@ s and t consist of lowercase English letters.
  * @param {string} t
  * @return {boolean}
  */
- var isAnagram = function(s, t) {
-    if(s.length !== t.length){
-        return false;
-    }
-    
-    const count = new Array(26);
-    count.fill(0);
-    
-    // ASCII value of 'a' is 97 and as all characters are in lowercase, we're subtracting ASCII value of 'a' to get the index to store the count of the character. e.g. for character 'a' index would be 0
-    for(let i = 0; i < s.length; i++){
-        count[s.charCodeAt(i) - 97]++;
-        count[t.charCodeAt(i) - 97]--;
-    }
-    
-    return !count.find(c => c !== 0);
-};
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
 
+  const count = new Array(26);
+  count.fill(0);
+
+  // ASCII value of 'a' is 97 and as all characters are in lowercase, we're subtracting ASCII value of 'a' to get the index to store the count of the character. e.g. for character 'a' index would be 0
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+
+  return !count.find((c) => c !== 0);
+};
 
 // Solution 2:
 
@@ -52,36 +51,28 @@ s and t consist of lowercase English letters.
  * @param {string} t
  * @return {boolean}
  */
- var isAnagramAnotherSolution = function(s, t) {
-    if(s.length !== t.length){
-        return false;
-    }
-    
-    const count = {};
-    
-    // to find out count of each character of 's'
-    for(const currentChar of s){                
-        if(count[currentChar]){
-            count[currentChar] += 1;
-        }else{
-            count[currentChar] = 1
-        }        
+const isAnagramAnotherSolution = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const count = {};
+
+  // to find out count of each character of 's'
+  for (const currentChar of s) {
+    count[currentChar] = (count[currentChar] || 0) + 1;
+  }
+
+  // then, reducing count for each character of 't' and after that if we get all values of count object as 0 i.e. 's', 't' are anagram of each other
+  for (let i = 0; i < t.length; i++) {
+    const currentChar = t[i];
+
+    if (!count[currentChar]) {
+      return false;
     }
 
-    // then, reducing count for each character of 't' and after that if we get all values of count object as 0 i.e. 's', 't' are anagram of each other
-    for(let i = 0; i < t.length; i++){
-        const currentChar = t[i];
-        
-        if(count[currentChar]){
-            count[currentChar] -= 1;
-        }       
-    }
-    
-    for(let key in count){
-        if(count[key] !== 0){
-            return false;
-        }
-    }
-    
-    return true;    
+    count[currentChar] -= 1;
+  }
+
+  return !Object.keys(count).some((key) => count[key] !== 0);
 };
