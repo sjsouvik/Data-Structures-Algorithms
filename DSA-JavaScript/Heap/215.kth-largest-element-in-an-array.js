72d712fd4e4baccd26f2ee44a6061ae35a027872
Souvik Jana <souvikjana2013@gmail.com>
2023-09-24 15:24:33 +0530

    Update the solution to find the Kth largest item in an array

--- DSA-JavaScript/Heap/215.kth-largest-element-in-an-array.js
+++ DSA-JavaScript/Heap/215.kth-largest-element-in-an-array.js
@@ -25,31 +25,22 @@ Constraints:
 
 *************************************************************Solution********************************************************/
 
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-
-class MinHeap {
-  constructor() {
+class PriorityQ {
+  constructor(compare) {
+    this.compare = compare;
     this.items = [];
   }
 
-  parent(index) {
-    return Math.floor((index - 1) / 2);
+  parent(i) {
+    return Math.floor((i - 1) / 2);
   }
 
-  left(index) {
-    return 2 * index + 1;
+  left(i) {
+    return 2 * i + 1;
   }
 
-  right(index) {
-    return 2 * index + 2;
-  }
-
-  getMin() {
-    return this.items[0];
+  right(i) {
+    return 2 * i + 2;
   }
 
   swap(index1, index2) {
@@ -59,52 +50,62 @@ class MinHeap {
     ];
   }
 
-  //Time complexity: O(height of the binary heap) = O(log n) since we are swapping the elements along the height of the binary heap
-  insert(item) {
+  peek() {
+    return this.items[0];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  add(item) {
     this.items.push(item);
 
     let i = this.items.length - 1;
-    while (i !== 0 && this.items[i] < this.items[this.parent(i)]) {
+    while (i !== 0 && this.compare(this.items[this.parent(i)], this.items[i])) {
       this.swap(i, this.parent(i));
       i = this.parent(i);
     }
   }
 
-  //Time complexity: O(log n) as we are swapping the elements along the height of the binary heap
-  minHeapify(index) {
-    const left = this.left(index);
-    const right = this.right(index);
-    const heapSize = this.items.length;
-    let smallest = index;
+  heapify(index) {
+    const left = this.left(index),
+      right = this.right(index);
+    let rootIndex = index;
 
-    if (left < heapSize && this.items[left] < this.items[smallest]) {
-      smallest = left;
+    if (
+      left < this.size() &&
+      this.compare(this.items[rootIndex], this.items[left])
+    ) {
+      rootIndex = left;
     }
 
-    if (right < heapSize && this.items[right] < this.items[smallest]) {
-      smallest = right;
+    if (
+      right < this.size() &&
+      this.compare(this.items[rootIndex], this.items[right])
+    ) {
+      rootIndex = right;
     }
 
-    if (smallest !== index) {
-      this.swap(index, smallest);
-      this.minHeapify(smallest);
+    if (rootIndex !== index) {
+      this.swap(rootIndex, index);
+      this.heapify(rootIndex);
     }
   }
 
-  extractMin() {
-    if (this.items.length === 0) {
+  poll() {
+    if (this.size() === 0) {
       return;
     }
 
-    if (this.items.length === 1) {
+    if (this.size() === 1) {
       return this.items.pop();
     }
 
     this.swap(0, this.items.length - 1);
-    const minItem = this.items.pop();
-    this.minHeapify(0);
-
-    return minItem;
+    const rootItem = this.items.pop();
+    this.heapify(0);
+    return rootItem;
   }
 }
 
@@ -130,22 +131,26 @@ if k === n, O(logn + nlogn) > O(logn)
 
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
 // Time complexity: O(klogk + 2(n - k)logk) = O(nlogk)
 const findKthLargest = function (nums, k) {
-  const minHeap = new MinHeap();
+  const minHeap = new PriorityQ((a, b) => a - b > 0);
 
-  // O(klogk) to build the heap with 'k' items
   for (let i = 0; i < k; i++) {
-    minHeap.insert(nums[i]);
+    minHeap.add(nums[i]);
   }
 
-  // (n - k) * (2logk) = 2(n - k)logk to extract and insert new item into the heap
   for (let i = k; i < nums.length; i++) {
-    if (nums[i] > minHeap.getMin()) {
-      minHeap.extractMin();
-      minHeap.insert(nums[i]);
+    if (nums[i] > minHeap.peek()) {
+      minHeap.poll();
+      minHeap.add(nums[i]);
     }
   }
 
-  return minHeap.getMin();
+  return minHeap.peek();
 };
