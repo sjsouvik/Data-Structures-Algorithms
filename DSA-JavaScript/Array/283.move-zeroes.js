173886cc7797b86d44476a80b16908c5f7ee635e
Souvik Jana <souvikjana2013@gmail.com>
2023-10-02 16:42:38 +0530

    Update 283.move-zeroes.js

--- DSA-JavaScript/Array/283.move-zeroes.js
+++ DSA-JavaScript/Array/283.move-zeroes.js
@@ -27,16 +27,13 @@ Constraints:
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var moveZeroes = function(nums) {
-    let noOfNonZero = 0, temp;
-    
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] !== 0){
-            temp = nums[i];
-            nums[i] = nums[noOfNonZero];
-            nums[noOfNonZero] = temp;
-            
-            noOfNonZero++;
-        }
+const moveZeroes = function (nums) {
+  let start = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[start], nums[i]] = [nums[i], nums[start]];
+      start++;
     }
+  }
 };
